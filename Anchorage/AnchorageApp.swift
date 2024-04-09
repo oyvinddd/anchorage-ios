38ebc1e82dce3ee02fcb3562cd17793a8e83d6dc
Øyvind Hauge <oyvind.s.hauge@gmail.com>
2024-04-09 22:03:03 +0200

    project scaffolding

---
+++ Anchorage/AnchorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnchorageApp.swift
+//  Anchorage
+//
+//  Created by Øyvind Hauge on 09/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnchorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Anchorage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Anchorage
+//
+//  Created by Øyvind Hauge on 09/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
